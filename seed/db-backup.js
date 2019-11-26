6cc3250a825912bb47d3ec4e4328134729e1b0a4
Muhammad Imran <imran@MacBook-Pro.local>
2019-11-26 17:24:15 +0300

    modified db backup script 2

--- seed/db-backup.js
+++ seed/db-backup.js
@@ -1,8 +1,8 @@
 var spawn = require('child_process').spawn;
 
 
-    var args = ['--db', 'heroku_141w6cdm', '--collection','events']
-      , mongodump = spawn('/usr/local/bin/mongodump', args);
+    var args = []
+      , mongodump = spawn('mongodump', args);
     mongodump.stdout.on('data', function (data) {
       console.log('stdout: ' + data);
     });
@@ -11,5 +11,14 @@ var spawn = require('child_process').spawn;
     });
     mongodump.on('exit', function (code) {
       console.log('mongodump exited with code ' + code);
+
+      const compressing = require('compressing');
+        compressing.tar.compressDir('dump', 'dump.tar')
+        .then(function(){
+            console.log('compression done');
+        })
+        .catch(function(){
+            console.log('compession not done');
+        });
     });
   
