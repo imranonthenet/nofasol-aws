ef8cfb7320c9580989f93c7ba7514e4bc706dd93
Muhammad Imran <imran@MacBook-Pro.local>
2019-11-29 17:55:09 +0300

    db backup

--- routes/event.js
+++ routes/event.js
@@ -1310,10 +1310,21 @@ router.get('/delete-all', function(req,res){
             Event.db.db.admin().command({repairDatabase:1}, function (err,result)
             {
                 if(err) throw err;
-
                 console.log(result);
+                
+                Event.db.db.admin().command({compact:heroku_141w6cdm}, function (err,result){
+                    if(err) throw err;
+                    console.log(result);
+                });
+
                 res.redirect('/event');
             });
+            /*
+            mongoose.connection.db.admin().command({setParameter: 1, internalQueryExecMaxBlockingSortBytes: 268435456}, function (err,result)
+            {
+                console.log(result);
+            });
+            */
 
             
         });
