9ab9ba2780bb8ddce7bfac5ae7ef7a5110885853
Muhammad Imran <imran@MacBook-Pro.local>
2019-11-29 11:12:00 +0300

    db backup

--- routes/backup.js
+++ routes/backup.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const spawn = require('child_process').spawn;
+const compressing = require('compressing');
+
 
 router.get('/', (req,res)=>{
     var messages=[];
     res.render('backup/index', {messages:messages});
+});
+
+router.get('/create', (req,res)=>{
+    var messages=[];
+
+    const args = [];
+    const mongodump = spawn('mongodump', args);
+
+    mongodump.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+    mongodump.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+    mongodump.on('exit', function (code) {
+        console.log('mongodump exited with code ' + code);
+
+      
+        compressing.tar.compressDir('dump', '../public/uploads/dump.tar')
+        .then(function(){
+            console.log('compression done');
+        })
+        .catch(function(){
+            console.log('compession not done');
+        });
+
+        res.render('backup/index', {messages:messages});
+    });
 });
 
 module.exports=router;
