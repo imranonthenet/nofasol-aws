244253b4b7322972103027f3c56acde5141fb89c
Muhammad Imran <imran@MacBook-Pro.local>
2019-12-01 10:06:52 +0300

    db backup

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,64 +1,34 @@
-$('.upload-btn').on('click', function (){
-    $('#upload-input').click();
-    $('.progress-bar').text('0%');
-    $('.progress-bar').width('0%');
-});
-
-$('#upload-input').on('change', function(){
-
-  var files = $(this).get(0).files;
-
-  if (files.length > 0){
-    // create a FormData object which will be sent as the data payload in the
-    // AJAX request
-    var formData = new FormData();
-
-    // loop through all the selected files and add them to the formData object
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
-
-      // add the files to formData object for the data payload
-      formData.append('uploads[]', file, file.name);
-    }
-
-    $.ajax({
-      url: '/backup/import',
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: function(data){
-          console.log('upload successful!\n' + data);
-          $('.progress-bar').html(data.result);
-      },
-      xhr: function() {
-        // create an XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-
-        // listen to the 'progress' event
-        xhr.upload.addEventListener('progress', function(evt) {
-
-          if (evt.lengthComputable) {
-            // calculate the percentage of upload completed
-            var percentComplete = evt.loaded / evt.total;
-            percentComplete = parseInt(percentComplete * 100);
-
-            // update the Bootstrap progress bar with the new percentage
-            $('.progress-bar').text(percentComplete + '%');
-            $('.progress-bar').width(percentComplete + '%');
-
-            // once the upload reaches 100%, set the progress bar text to done
-            if (percentComplete === 100) {
-              $('.progress-bar').html('Done');
-            }
-
-          }
-
-        }, false);
-
-        return xhr;
+$('#fileupload').fileupload({
+  dataType: 'json',
+  url: '/backup/import/',
+  autoUpload: true,
+  add: function (e, data) {
+      var goUpload = true;
+      var uploadFile = data.files[0];
+      if (!(/\.(xls|xlsx)$/i).test(uploadFile.name)) {
+          alert('You can only import Excel (xls or xlsx) files');
+          goUpload = false;
       }
-    });
+      if (uploadFile.size > 90000000) { // 10mb
+          alert('File size cannot be more than 90 mb');
+          goUpload = false;
+      }
+      if (goUpload == true) {
+          data.submit();
+      }
+  },
+  done: function (e, data) {
+      //location.reload(true);
+      console.log(data.result.result);
+      window.location='/event';
+      
 
+  }
+}).on('fileuploadprogressall', function (e, data) {
+  var progress = parseInt(data.loaded / data.total * 100, 10);
+  $('.progress .progress-bar').css('width', progress + '%');
+
+  if (progress === 100) {
+    $('.progress-bar').html('Please wait while data is being processed...');
   }
 });
